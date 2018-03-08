e5eaba87485a71bfb250554cc3660bf04efb23c2
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-03-08 11:34:51 +0000

    Removed dependency on javascript code to generate background images for headers on all pages (it generated too much network traffic and was needlesly complicated) - used a simple <picture> element with an absolute positioning for the page title and overlay in pseudo after element. All is now easily configurable from the css. Applied above to all existing pages. Created necessary global styles for the above in style.scss. Moved .page__title-container class to global styles at the top of styles.scss. Removed unused javascript code from site.js. Put that code into snippets.js - may be usefull one day.

--- static/js/site.js
+++ static/js/site.js
@@ -15,12 +15,6 @@ $(document).ready(function () {
     navigationButtonClose.on("click", closeNav);
 });
 
-
-function readDeviceOrientation() {
-
-    return window.matchMedia("(orientation: portrait)").matches;
-}
-
 function stopBodyScrolling (bool) {
     /**
      * Required to stop body scrolling on iOS devices (safari ignores
@@ -71,45 +65,6 @@ function closeNav() {
 
 function showCloseButton() {
     buttonCloseContainer.css("display", "inline");
-}
-
-// Responsive background image constructor.
-class ResponsiveBackgroundImage {
-
-    constructor(element) {
-        this.element = element;
-        this.img = element.querySelector('.responsive');
-        this.src = '';
-
-        this.img.addEventListener('load', () => {
-            this.update();
-        });
-
-        if (this.img.complete) {
-            this.update();
-        }
-    }
-
-    update() {
-        let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
-        let image = this.img;
-        let overlay = image.getAttribute("data-overlay");
-        if (overlay === null || overlay === undefined) {
-            overlay = "";
-        }
-
-        if (this.src !== src) {
-            this.src = src;
-
-            this.element.style.backgroundImage = overlay + ', url("' + this.src + '")';
-        }
-    }
-}
-
-// Create responsive background images.
-let elements = document.querySelectorAll('[data-image-type="responsive"]');
-for (let i=0; i<elements.length; i++) {
-    new ResponsiveBackgroundImage(elements[i]);
 }
 
 // Add active class to navigation elements in the navigation overlay and
