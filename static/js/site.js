2b569b3754b7f08d9f9051270fc85facb7247242
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-03-28 21:19:17 +0100

    Separated nav links away from each other a bit more. Removed unused javascript code from site.js Moved unused js code into snippets.js.

--- static/js/site.js
+++ static/js/site.js
@@ -1,27 +1,3 @@
-var buttonOpenContainer = $(".navigation__button-open-container");
-var hamburgerBar = $(".navigation__hamburger-bar");
-var navigationOverlay = $(".navigation__overlay");
-var buttonCloseContainer = $(".navigation__button-close-container");
-var navigationButtonClose = $(".navigation__button-close");
-var scrollPosition = 0;
-var scrollBarWidth = 0;
-var parallelSection = false;
-var preventDefaultAction = function(e) {
-    e.preventDefault();
-};
-
-
-$(document).ready(function () {
-    buttonOpenContainer.on("click", openNav);
-    navigationButtonClose.on("click", closeNav);
-});
-
-
-
-function removeChapters () {
-    o
-}
-
 // Code for active links and scrolling
 $(document).ready(function() {
     $(".navbar-nav .nav-item .nav-link").on("click", function(){
@@ -42,15 +18,15 @@ $(document).ready(function() {
             bottom = top + $(this).outerHeight();
 
             if (cur_pos >= top && cur_pos <= bottom) {
-                nav.find('a').removeClass('active');
-                sections.removeClass('active');
+                if ($(window).width() >= 992 && this.id === "chapters") {
+                //    pass - on large screens events & chapters are parallel,
+                //    ignore chapters section then
+                } else {
+                    nav.find('a').removeClass('active');
+                    sections.removeClass('active');
 
-                if ($(window).width() < 992) {
                     $(this).addClass('active');
-                    nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-                } else if (this.id !== "chapters") {
-                    $(this).addClass('active');
-                    nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+                    nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
                 }
             }
         });
@@ -93,82 +69,6 @@ $(document).ready(function() {
             }
         });
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function stopBodyScrolling (bool) {
-    /**
-     * Required to stop body scrolling on iOS devices (safari ignores
-     * body overflow: hidden in css.
-     */
-    if (bool === true) {
-        document.body.addEventListener("touchmove", preventDefaultAction, false);
-    } else {
-        document.body.removeEventListener("touchmove", preventDefaultAction, false);
-    }
-}
-
-function setActiveLink() {
-    var currentURL = window.location.href;
-    $('.navigation__link, .footer__link').each(function () {
-        if (this.href === currentURL) {
-            $(this).addClass('active');
-        }
-    });
-}
-
-function openNav() {
-    scrollPosition = $(window).scrollTop();
-    scrollBarWidth = (window.innerWidth - $(window).width());
-    $(".body").addClass("no-scroll");
-    stopBodyScrolling(true);
-    // To prevent content from jumping when scrollbar disappears.
-    $(window).scrollTop(scrollPosition);
-    $(".body").css("padding-right", scrollBarWidth + "px");
-    buttonOpenContainer.css("pointer-events", "none");
-    buttonOpenContainer.toggle(500, showCloseButton);
-    hamburgerBar.css("background-color", "transparent");
-    navigationOverlay.css("width", "100%");
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    $(".body").removeClass("no-scroll");
-    stopBodyScrolling(false);
-    // To stop content jerking left-right when scrollbar is restored.
-    $(".body").css("padding-right", "");
-    hamburgerBar.css("background-color", "#ffffff");
-    navigationOverlay.css("width", "0%");
-    buttonOpenContainer.css("display", "inline");
-    buttonOpenContainer.css("pointer-events", "auto");
-    buttonCloseContainer.css("display", "none");
-}
-
-function showCloseButton() {
-    buttonCloseContainer.css("display", "inline");
-}
-
-// Add active class to navigation elements in the navigation overlay and
-// footer on page load.
-$(setActiveLink());
 
 
 
